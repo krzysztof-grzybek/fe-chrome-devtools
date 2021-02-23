e56b6c2a53a1d7d550f3a8bd6bc868487b3b22c6
krzysztof-grzybek <krzysztot.grzybek89@gmail.com>
2021-02-23 19:33:45 +0100

    console example

--- basic-example/main.js
+++ basic-example/main.js
@@ -1,1 +1,11 @@
 console.log('Hello world!')
+
+const islands = ['/basic-example/seyschelles.jpg', '/basic-example/maledives.jpg', '/basic-example/bali.jpg'];
+
+const img = document.getElementById('island');
+const button = document.getElementById('change-island');
+
+button.addEventListener('click', () => {
+    const randomIndex = Math.round(Math.random() * 3);
+    img.src = islands[randomIndex];
+});
